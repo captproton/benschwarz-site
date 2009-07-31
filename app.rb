f5f4e72507a999a76e3ff7a5da8767ac529f5343
Ben Schwarz <ben.schwarz@gmail.com>
2009-07-31 18:36:52 +1000

    Updated dependencies. Temporarily hidden articles. Added a live flickr image.

--- app.rb
+++ app.rb
@@ -28,16 +28,33 @@ module Germanforblack
       def flickr_url(photo)
         "http://flickr.com/photos/benschwarz/#{photo[:id]}"
       end
+      
+      def entity_encode(string)
+        output_array = []
+        lower = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+        upper = %w(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
+        char_array = string.split('')
+        char_array.each do |char|  
+          output = lower.index(char) + 97 if lower.include?(char)
+          output = upper.index(char) + 65 if upper.include?(char)
+          if output
+            output_array << "&##{output};"
+          else 
+            output_array << char
+          end
+        end
+        return output_array.join
+      end
     end
     
     get '/' do
       @page_id = 'home'
       
       @links = Smoke[:delicious].output
-      @event = Smoke[:upcoming].output 
+      @event = Smoke[:upcoming].output.first
       @projects = Smoke[:github].output
       @presentations = Smoke[:slideshare].output
-      @twitter = Smoke[:twitter].output
+      @twitter = Smoke[:twitter].output.first
       @images = Smoke[:flickr].output
       @articles = Article.all.sort
       

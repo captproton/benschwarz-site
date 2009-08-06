44de70463c744d821d3ffd2cf940e6d3e415fbdd
Ben Schwarz <ben.schwarz@gmail.com>
2009-08-06 23:32:31 +1000

    Use memcached as the default cache store and fall back to using the memory hash storage if an exception occurs.

--- lib/cache.rb
+++ lib/cache.rb
@@ -1,7 +1,13 @@
 module Germanforblack
   unless defined?(Cache)
     require 'moneta'
-    require 'moneta/memory'
-    Cache = Moneta::Memory.new
+    begin
+      require 'moneta/memcache'
+      Cache = Moneta::Memcache.new(:server => ENV['MEMCACHE_SERVERS'].split(','), :namespace => ENV['MEMCACHE_NAMESPACE'])
+    rescue
+      puts "*** Using memory cache store"
+      require 'moneta/memory'
+      Cache = Moneta::Memory.new
+    end
   end
 end

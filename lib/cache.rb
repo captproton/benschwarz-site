25ad21cec4cce5ca5f64918e5989b67364d99cb5
Ben Schwarz <ben.schwarz@gmail.com>
2009-08-12 22:31:51 +1000

    Removing memory version of caching

--- lib/cache.rb
+++ lib/cache.rb
@@ -1,14 +1,7 @@
 module Germanforblack
   unless defined?(Cache)
     require 'moneta'
-    begin
-      puts "*** Using memcached cache store"
-      require 'moneta/memcache'
-      Cache = Moneta::Memcache.new(:server => ENV['MEMCACHE_SERVERS'].split(','), :namespace => ENV['MEMCACHE_NAMESPACE'])
-    rescue
-      puts "*** Using memory cache store"
-      require 'moneta/memory'
-      Cache = Moneta::Memory.new
-    end
+    require 'moneta/memcache'
+    Cache = Moneta::Memcache.new(:server => ENV['MEMCACHE_SERVERS'].split(','), :namespace => ENV['MEMCACHE_NAMESPACE'])
   end
 end
